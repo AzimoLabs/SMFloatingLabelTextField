e780fbdc513240be37480576541e79e9ec7bb941
Oleksandr Masliukivskyi <oleksandr.masliukivskyi@azimo.com>
2021-03-30 15:53:11 +0200

    fixed path issue

--- Package.swift
+++ Package.swift
@@ -20,9 +20,12 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SMFloatingLabelTextField",
-            dependencies: []),
+            dependencies: [],
+            path: "SMFloatingLabelTextField",
+            exclude: ["Sources/SMFloatingLabelTextField/SMFloatingLabelTextField.swift"]),
         .testTarget(
             name: "SMFloatingLabelTextFieldTests",
-            dependencies: ["SMFloatingLabelTextField"]),
+            dependencies: ["SMFloatingLabelTextField"],
+            path: "SMFloatingLabelTextField"),
     ]
 )
