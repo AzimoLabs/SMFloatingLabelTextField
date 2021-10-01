a783d8df0fa795e0b9898c89944becf1037a634d
Miroslaw Stanek <froger.mcs@gmail.com>
2021-10-01 14:51:55 +0200

    Example viewcontroller is rewritten to swfit

---
+++ Example/SMFloatingLabelTextField/MainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainViewController.swift
+//  SMFloatingLabelTextField_Example
+//
+//  Created by Miroslaw Stanek on 01/10/2021.
+//  Copyright © 2021 Michał Moskała. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SMFloatingLabelTextField
+
+class MainViewController: UIViewController {
+    
+    @IBOutlet weak var lastName: SMFloatingLabelTextField!
+    @IBOutlet weak var firstName: SMFloatingLabelTextField!
+    @IBOutlet weak var address: SMFloatingLabelTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.lastName.attributedPlaceholder = NSAttributedString.init(string: "Last name text",
+                                                                      attributes: [
+                                                                        NSAttributedString.Key.foregroundColor: UIColor.magenta,
+                                                                        NSAttributedString.Key.font: UIFont.systemFont(ofSize: 14.0, weight: UIFont.Weight.bold),
+                                                                        NSAttributedString.Key.underlineStyle: NSUnderlineStyle.single
+                                                                    ])
+        self.address.text = "Main Square, Kraków, Poland"
+    }
+}
